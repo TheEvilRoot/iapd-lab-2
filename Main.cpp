7d4852714a6b8bedf918c3b73bb0d0671523dd09
TheEvilRoot <robot-creatix@ya.ru>
2020-09-29 04:01:26 +0300

    Add volumes index and info per device

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include <optional>
 #include <memory>
 #include <array>
+#include <vector>
 
 #include <Windows.h>
 
@@ -14,6 +15,14 @@ struct DeviceInfo {
   std::string firmware_version;
   std::string bus_type;
   std::string serial_number;
+};
+
+struct Volume {
+  char letter;
+  DWORD deviceNumber;
+  _ULARGE_INTEGER totalSpace;
+  _ULARGE_INTEGER freeSpace;
+  _ULARGE_INTEGER busySpace;
 };
 
 std::array<std::string, 16> busTypes{
@@ -53,12 +62,46 @@ std::optional<DeviceInfo> acquireDeviceInfo(HANDLE driveHandle, STORAGE_PROPERTY
   return info;
 }
 
+auto indexVolumes(STORAGE_PROPERTY_QUERY& query) {
+  std::vector<Volume> index;
+  auto logicalDrivesCount = GetLogicalDrives();
+
+  for (char letter = 'A'; letter <= 'Z'; letter++) {
+    if ((logicalDrivesCount >> letter - 'A') & 1 && letter) { // what the fuck?
+      std::string path = letter + ":\\";
+      Volume vol;
+      vol.letter = letter;
+      GetDiskFreeSpaceExA(path.c_str(), 0, &vol.totalSpace, &vol.freeSpace);
+
+      path = std::string("\\\\.\\") + std::string(1, letter) + ":";
+
+      auto volumeHandle = CreateFileA(path.c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, nullptr);
+      if (volumeHandle == INVALID_HANDLE_VALUE) {
+        std::cerr << "indexVolumes: " << letter << ": " << GetLastError() << "\n";
+      }
+
+      auto buffer = std::make_unique<char[]>(12);
+      if (!DeviceIoControl(volumeHandle, IOCTL_STORAGE_GET_DEVICE_NUMBER, &query, sizeof(query), buffer.get(), 12, nullptr, nullptr)) {
+        continue;
+      }
+
+      const _STORAGE_DEVICE_NUMBER* deviceNumber = reinterpret_cast<_STORAGE_DEVICE_NUMBER*>(buffer.get());
+      vol.deviceNumber = deviceNumber->DeviceNumber;
+      vol.busySpace.QuadPart = vol.totalSpace.QuadPart - vol.freeSpace.QuadPart;
+      index.push_back(vol);
+    }
+  }
+  return index;
+}
+
 int main() {
   STORAGE_PROPERTY_QUERY query;
   std::memset(&query, 0, sizeof(query));
 
   query.PropertyId = StorageDeviceProperty;
   query.QueryType = PropertyStandardQuery;
+
+  auto volumeIndex = indexVolumes(query);
 
   HANDLE driveHandle{ INVALID_HANDLE_VALUE };
   for (uint32_t index = 0; true; index++) {
@@ -71,9 +114,15 @@ int main() {
         std::cout << "Firmware: " << i.firmware_version << "\n";
         std::cout << "Bus type: " << i.bus_type << "\n";
         std::cout << "Serial number: " << i.serial_number << "\n";
+
+        std::cout << " Volumes: \n";
+        for (const auto vol : volumeIndex) {
+          if (vol.deviceNumber == index) {
+            std::cout << "  " << vol.letter << ": \n   Total: " << vol.totalSpace.QuadPart << "\n   Free: " << vol.freeSpace.QuadPart << "\n   Busy: " << vol.busySpace.QuadPart << "\n\n";
+          }
+        }
       }
     } else break;
   }
-
 
 }
